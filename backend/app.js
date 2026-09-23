ec35ea1816df68f68396c6ab1c5e88b29a8e5116
huiiimmm <donghuiimm@gmail.com>
2026-09-23 14:33:39 +1000

    syntax fixes

--- backend/app.js
+++ backend/app.js
@@ -12,10 +12,10 @@ app.get('/api/health', (req, res) => res.json({ status: 'ok' }));
 app.use('/auth', require('./routes/auth'));
 app.use('/api/capsules', require('./routes/capsules'));
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
 });
 
 module.exports = app;
